04aba5b2e96e1bd58ae061ee48ede526a8d65dc7
JanMzA <janmeyerzuallendorf@yahoo.de>
2022-10-08 18:30:51 +0200

    Fertig

--- TicTacToe.py
+++ TicTacToe.py
@@ -1,48 +1,56 @@
 import random as rd
 
+
+def change_player_symbol(current_symbol):
+    if current_symbol == "X":
+        next_symbol = "O"
+
+    else:
+        next_symbol = "X"
+
+    return next_symbol
+
+
+def eingabe_position():
+    try:
+        row = int(input("Geben Sie die Reihe ein: "))
+        col = int(input("Geben Sie die Spalte ein: "))
+        return row, col
+
+    except:
+        print("Geben Sie Zahlen ein")
+        TicTacToe().eingabe_position()
+
+
 class TicTacToe:
 
-
-    def __init__(self, win = False,groesse = 3):
+    def __init__(self, win=False, groesse=3):
         self.groesse = groesse
-        self.spielfeld = [["_" for i in range(groesse)]for j in range(groesse)]
+        self.spielfeld = [["_" for i in range(groesse)] for j in range(groesse)]
         self.win = win
 
     def print_spielfeld(self):
         for row in self.spielfeld:
             print(*row)
 
-    def check_zug_gueltig(self,row,col):
-        if row > 2 or col >2 or self.spielfeld[row][col] != "_":
+    def check_zug_gueltig(self, row, col):
+        if row > 2 or col > 2 or self.spielfeld[row][col] != "_":
             print("ungültige Eingabe, Eingabe wiederholen")
             return False
 
         if self.spielfeld[row][col] == "_":
             return True
 
-    def eingabe_position(self):
-        try:
-            row = int(input("Geben Sie die Reihe ein: "))
-            col = int(input("Geben Sie die Spalte ein: "))
-            return row, col
-        except:
-            print("Geben Sie Zahlen ein")
-            TicTacToe().eingabe_position()
+    def set_stone(self, row, col, player_symbol):
+        self.spielfeld[row][col] = player_symbol
 
-
-    def set_stone(self,row,col,player_symbol):
-        (self.spielfeld)[row][col] = player_symbol
-
-
-
-    def check_win(self,player_symbol):
+    def check_win(self, player_symbol):
         directions = ["row", "col", "diagonal"]
         break_out_dir = False
 
-
         for dir in directions:
 
-            if break_out_dir == True:
+            if break_out_dir:
                 break
 
             if dir == "row":
@@ -78,68 +86,41 @@ class TicTacToe:
                             break_out_dir = True
                             break
 
-        if self.win == True:
+        if self.win:
             print("Player " + player_symbol + " hat gewonnen")
-
 
         return self.win
 
-
-
-    def change_player_symbol(self,current_symbol):
-
-        if current_symbol == "X":
-            next_symbol = "O"
-
-        else:
-            next_symbol = "X"
-
-        return next_symbol
-
-
-
     def check_spielfeld_voll(self):
-        if "_" not in self.spielfeld[0] and "_" not in self.spielfeld[1] and "_"  not in self.spielfeld[2]:
+        if "_" not in self.spielfeld[0] and "_" not in self.spielfeld[1] and "_" not in self.spielfeld[2]:
             self.win = True
 
-        if self.win == True:
+        if self.win:
             print("Spiel endet unentschieden")
         return self.win
 
 
-
-
 Spielfeld = TicTacToe()
-symbole =["X","O"]
+symbole = ["X", "O"]
 
-player_symbol = symbole[rd.randint(0,1)]
+player_symbol = symbole[rd.randint(0, 1)]
 
 Spielfeld.print_spielfeld()
-while Spielfeld.check_win(player_symbol) == False:
+
+while not Spielfeld.check_win(player_symbol):
     print(player_symbol + " ist an der Reihe")
     eingabegueltig = False
-    while eingabegueltig == False:
+    while not eingabegueltig:
         row, col = Spielfeld.eingabe_position()
-        eingabegueltig = Spielfeld.check_zug_gueltig(row,col)
+        eingabegueltig = Spielfeld.check_zug_gueltig(row, col)
     Spielfeld.set_stone(row, col, player_symbol)
     win = Spielfeld.check_win(player_symbol)
-    if win == True:
+    if win:
         break
     Spielfeld.print_spielfeld()
-    player_symbol = Spielfeld.change_player_symbol(player_symbol)
+    player_symbol = change_player_symbol(player_symbol)
     full = Spielfeld.check_spielfeld_voll()
     if full:
         break
 
-
-
-### Konsole zum spielen öffnen (pygame)
-
-
-
-
-
-
-
-
-
+# Konsole zum Spielen öffnen (pygame)
